ae6ccf98e2bf7a5e2c8e2f029393fa47c4666051
Annabelle Heaford <annabelle@gibedigital.com>
2018-03-01 15:08:30 +0000

    Include CaptchaValidatorAttribute

---
+++ src/Recaptcha.Web/Mvc/CaptchaValidatorAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Recaptcha.Web.Mvc
+{
+	public class CaptchaValidatorAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext ctx)
+		{
+			ctx.ActionParameters["captchaValid"] = false;
+
+			var validator = (ctx.Controller as Controller).GetRecaptchaVerificationHelper();
+
+			if (string.IsNullOrEmpty(validator.Response))
+			{
+				return;
+			}
+
+			if (validator.VerifyRecaptchaResponse() == RecaptchaVerificationResult.Success)
+			{
+				ctx.ActionParameters["captchaValid"] = true;
+			}
+		}
+	}
+}
